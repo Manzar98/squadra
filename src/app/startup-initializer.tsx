02cf757a40e1ad6963b2a623e8fb059e9cf5b86b
Manzar98 <manzarnouman@gmail.com>
2025-09-23 15:58:42 +0500

    fix: removed flowzone

--- src/app/startup-initializer.tsx
+++ src/app/startup-initializer.tsx
@@ -3,11 +3,9 @@
 import { useEffect } from 'react'
 import clarity from '@microsoft/clarity'
 import { useLoadSkills } from '../hooks/loadSkills'
-import { useLoadFlowZones } from '../hooks/loadFlowZones'
 
 export const StartupInitializer = () => {
   useLoadSkills()
-  useLoadFlowZones()
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
